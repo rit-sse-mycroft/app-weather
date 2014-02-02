094a99d5e148a6e244e278bfac64816a73747f6a
kristenmills <kristen@kristen-mills.com>
2014-02-01 22:48:56 -0500

    Updated to use new ruby gem

--- weather.rb
+++ weather.rb
@@ -16,52 +16,46 @@ class Weather < Mycroft::Client
     super
   end
 
-  def connect
-    # Your code here
-  end
-
-  def on_data(parsed)
-    if parsed[:type] == 'MSG_BROADCAST'
-      barometer = Barometer.new('14623')
-      weather = barometer.measure
-      grammar = parsed[:data]['content']
-      unless grammar.nil?
-        if grammar['grammar'] == 'weather'
-          tags = grammar['tags']
-          if tags['day'] == 'currently'
-            current(weather)
-          elsif not tags['prefix'].nil?
-            if tags['prefix'] == 'when is'
-              sunrise_sunset(weather, tags['day'], tags['rise_or_set'])
-            else
-              until_sunrise_sunset(weather, tags['day'], tags['rise_or_set'])
-            end
+  on 'MSG_BROADCAST' do |data|
+    barometer = Barometer.new('14623')
+    weather = barometer.measure
+    grammar = data['content']
+    unless grammar.nil?
+      if grammar['grammar'] == 'weather'
+        tags = grammar['tags']
+        if tags['day'] == 'currently'
+          current(weather)
+        elsif not tags['prefix'].nil?
+          if tags['prefix'] == 'when is'
+            sunrise_sunset(weather, tags['day'], tags['rise_or_set'])
           else
-            today_tomorrow(weather, tags['day'])
+            until_sunrise_sunset(weather, tags['day'], tags['rise_or_set'])
           end
-        end
-      end
-    elsif parsed[:type] == 'APP_DEPENDENCY'
-      if not parsed[:data]['stt'].nil?
-        update_dependencies(parsed[:data])
-        puts "Current status of dependencies"
-        puts @dependencies
-        if not @dependencies['stt'].nil?
-          if @dependencies['stt']['stt1'] == 'up' and not @sent_grammar
-            up
-            data = {grammar: { name: 'weather', xml: File.read('./grammar.xml')}}
-            query('stt', 'load_grammar', data)
-            @sent_grammar = true
-          elsif @dependencies['stt']['stt1'] == 'down' and @sent_grammar
-            @sent_grammar = false
-            down
-          end
+        else
+          today_tomorrow(weather, tags['day'])
         end
       end
     end
   end
 
-  def on_end
+  on 'APP_DEPENDENCY' do |data|
+    update_dependencies(data)
+    puts "Current status of dependencies"
+    puts @dependencies
+    if not @dependencies['stt'].nil?
+      if @dependencies['stt']['stt1'] == 'up' and not @sent_grammar
+        up
+        data = {grammar: { name: 'weather', xml: File.read('./grammar.xml')}}
+        query('stt', 'load_grammar', data)
+        @sent_grammar = true
+      elsif @dependencies['stt']['stt1'] == 'down' and @sent_grammar
+        @sent_grammar = false
+        down
+      end
+    end
+  end
+
+  on 'end' do
     query('stt', 'unload_grammar', {grammar: 'weather'})
   end
 end
